40461df86416682eb33ad66e4272b1e91d74bfe7
Alan Souza Fagundes <alan.souza@solutis.com.br>
2020-12-26 00:36:37 -0300

    Total update

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import '../styles/globals.scss'
 
-
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
-config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
-
-
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
 export default App
+  
--- pages/index.js
+++ pages/index.js
@@ -10,57 +10,61 @@ const Home = () => {
 	return (
 		<div className={`${styles.container}`}>
 			<Head>
-				<title>Alan</title>
+				<title>Hi, i'm Alan</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main className={styles.main}>
-				<div className={styles.profile}>
-					<div className={`${styles.title}`}>
-						<h1 className={styles.hey}>Olá </h1>
-						<h1 className={styles.saudation}>Meu nome é Alan</h1>
-						<span className={`${styles.icon} ${styles.glow}`}> ✦ </span>
+				<section className={styles.principal}>
+					<div className={styles.title}>
+						<h4> Olá, meu nome é </h4>
+						<h1> Alan </h1>
 					</div>
-					<div className={styles.bio}>Desenvolvedor de software.</div>
-					<div className={styles.chips}>
+
+					<div className={styles.interest}>
 						<span className={styles.chip}> Python </span>
 						<span className={styles.chip}> JavaScript </span>
 						<span className={styles.chip}> React </span>
 						<span className={styles.chip}> Data Sience </span>
 						<span className={styles.chip}> Nlp </span>
 					</div>
-				</div>
-				<div className={styles.info}>
-					<p className={styles.jobs}>
-						Formado em engenharia de software pela <a href="https://www.ucsal.br/"> Ucsal</a>. <br />
-						Atualmente sou desenvolvedor na <a href="https://solutis.com.br/">Solutis</a>.
-						<br />
-						Tenho <strong>{parseInt(today.getFullYear()) - 1998}</strong> anos e sou de
-						<strong> Salvador, Bahia, Brasil.</strong>
-						<br />
-					</p>
 
-					<div className={styles.comunications}>
-						<a href="https://github.com/alanfgn" target="_blank">
+					<div className={styles.text}>
+						Sou desenvolvedor de software, formado em engenharia de software pela{" "}
+						<a href="https://www.ucsal.br/"> Ucsal</a>, atualmente desenvolvo na{" "}
+						<a href="https://solutis.com.br/">Solutis</a>, tenho {parseInt(today.getFullYear()) - 1998} anos
+						e sou de Salvador, Bahia, Brasil. Amo gatos e acho o ceu noturno muito bonito.
+					</div>
+
+					<div className={styles.contacts}>
+						<a href="https://github.com/alanfgn" target="_blank" title="Github">
 							<FontAwesomeIcon icon={faGithub} />
 						</a>
 
-						<a href="https://twitter.com/alanfgn" target="_blank">
+						<a href="https://twitter.com/alanfgn" target="_blank" title="Twitter">
 							<FontAwesomeIcon icon={faTwitter} />
 						</a>
 
-						<a href="https://www.linkedin.com/in/alan-souza-fagundes/" target="_blank">
+						<a href="https://www.linkedin.com/in/alan-souza-fagundes/" target="_blank" title="Linkedin">
 							<FontAwesomeIcon icon={faLinkedin} />
 						</a>
 
 						<a href="https://t.me/alanfgn">
-							<FontAwesomeIcon icon={faTelegramPlane} target="_blank" />
+							<FontAwesomeIcon icon={faTelegramPlane} target="_blank" title="Telegram" />
 						</a>
 
-						<a href="mailto:alan.souza.fagundes@gmail.com" target="_blank">
+						<a href="mailto:alan.souza.fagundes@gmail.com" target="_blank" title="Email">
 							<FontAwesomeIcon icon={faEnvelope} />
 						</a>
 					</div>
+				</section>
+
+				<div className={styles.sky}>
+					{[...Array(200).keys()].map(i => (
+						<span key={i} className={`${styles.particle} ${styles.glow}`}>
+							✦
+						</span>
+					))}
 				</div>
 			</main>
 
